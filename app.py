e88c4e61dc585b2bd8a54ff82f7a32e6504e0d99
Shoham Das <79916284+Shoh4685@users.noreply.github.com>
2026-02-02 21:12:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import yt_dlp
 # --- Page Config ---
 st.set_page_config(
     page_title="TubeLoader", 
-    page_icon="📥", 
+    page_icon="Page icon.png", 
     layout="centered"
 )
 
@@ -146,3 +146,4 @@ if video_url or fetch_button:
 # --- Footer ---
 st.markdown("<br><br><br>", unsafe_allow_html=True)
 st.markdown("<center style='opacity: 0.5; font-size: 0.8rem;'>Made with not ❤️ but hate for capitalism</center>", unsafe_allow_html=True)
+
