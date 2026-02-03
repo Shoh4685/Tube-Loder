a7e4c2b4e75db5aa879dffc766414cfa53de3df4
Shoham Das <79916284+Shoh4685@users.noreply.github.com>
2026-02-03 14:43:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import yt_dlp
 # --- Page Config ---
 st.set_page_config(
     page_title="TubeLoader", 
-    page_icon="⚖️", 
+    page_icon="♬", 
     layout="centered"
 )
 
@@ -275,5 +275,6 @@ with col_main:
 # --- Footer ---
 st.markdown("<br><br><br>", unsafe_allow_html=True)
 st.markdown("<center class='footer-text'>FORM FOLLOWS FUNCTION</center>", unsafe_allow_html=True)
+
 
 
